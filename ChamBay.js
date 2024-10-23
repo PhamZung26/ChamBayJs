161c4c6f16f0ac45409f94d19e8f80331e58695d
PhamZung26 <37917861+PhamZung26@users.noreply.github.com>
2024-10-23 14:52:43 +0700

    Update ChamBay.js

--- ChamBay.js
+++ ChamBay.js
@@ -2,18 +2,21 @@
 // @name         Auto Detect Position Frame
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Tự động kiểm tra và tìm iframe và div position_frame mỗi khi người dùng tải trang
-// @author       Phạm Dũng
-// @match        *://*/*    // Điều chỉnh URL này theo trang web cụ thể của bạn
+// @description  Tự động kiểm tra và tìm iframe và div position_frame trên mọi trang, bao gồm các trang với địa chỉ IP
+// @author       Bạn
+// @match        *://*/*       // Chạy trên tất cả các trang web bao gồm địa chỉ IP
+// @match        http://10.0.0.189:8080/*
 // @grant        none
 // ==/UserScript==
+'use strict';
+let intervalID; // Khai báo intervalID trước
 
-(function() {
-    'use strict';
+function tryGetPositionFrame() {
+    let iframe = document.getElementsByName('iframe1')[0];
 
-    function tryGetPositionFrame() {
-        let iframe = document.querySelector('iframe[name="iframe1"]');
-        if (iframe) {
+    if (iframe) {
+        // Kiểm tra xem iframe đã tải xong hay chưa
+        if (iframe.contentDocument || iframe.contentWindow.document) {
             let iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
             let positionFrame = iframeDocument.getElementById('position_frame');
 
@@ -22,13 +25,35 @@
                 positionFrame.style.overflowX = 'auto'; // Thêm cuộn ngang
                 positionFrame.style.whiteSpace = 'nowrap'; // Giữ tất cả các phần tử trên một dòng
                 // Thực hiện các thao tác với positionFrame
-                clearInterval(intervalID);  // Dừng kiểm tra khi đã tìm thấy
+                // Dừng kiểm tra khi đã tìm thấy
+                clearInterval(intervalID);
             } else {
                 console.log("Chưa tìm thấy position_frame, đang kiểm tra lại...");
             }
+        } else {
+            console.log("Iframe chưa tải xong.");
         }
+    } else {
+        console.log("Iframe không tồn tại.");
     }
+}
 
-    // Lặp lại việc kiểm tra mỗi 500ms cho đến khi tìm thấy position_frame
-    let intervalID = setInterval(tryGetPositionFrame, 500);
-})();
+// Kiểm tra iframe có tải xong chưa và chạy mã sau khi iframe đã tải xong
+function runScriptAfterIframeLoaded() {
+    let iframe = document.getElementsByName('iframe1')[0];
+
+    if (iframe) {
+        // Thêm sự kiện onload vào iframe
+        iframe.onload = function() {
+            console.log("Iframe đã tải xong.");
+            // Sau khi iframe tải xong, bắt đầu quá trình kiểm tra position_frame
+            intervalID = setInterval(tryGetPositionFrame, 500);
+        };
+    } else {
+        console.log("Không tìm thấy iframe.");
+    }
+}
+
+// Gọi hàm để bắt đầu kiểm tra khi iframe đã sẵn sàng
+runScriptAfterIframeLoaded();
+
